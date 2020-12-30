5bb436f4fe74e6a4b9a2e42e94713caa4d8689e5
prathamesh22davande <prathamesh22davande@gmail.com>
2020-12-30 19:08:46 +0530

    Applied Validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Employee } from './employee';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,11 @@ export class AppComponent {
   title = 'template-driven-form';
 
   positions = ['Manager', 'Developer', 'Tester'];
+
+  employeeModel = new Employee(
+    'Prathamesh',
+    'p@test.com',
+    '880601070',
+    'Manager'
+  );
 }
